9cf7402eedc573ad4b658c81b09c2c42173d911a
KeePassium <info@keepassium.com>
2019-11-10 14:32:57 +0100

    Fix: compatibility with other installed KeePass apps

--- KeePassiumLib/KeePassiumLib/files/FileType.swift
+++ KeePassiumLib/KeePassiumLib/files/FileType.swift
@@ -15,6 +15,7 @@ public enum FileType {
     public static let databaseUTIs = [
         "public.data", "public.content", // Google Drive needs this
         "com.keepassium.kdb", "com.keepassium.kdbx",
+        "com.maxep.mikee.kdb", "com.maxep.mikee.kdbx", 
         "com.jflan.MiniKeePass.kdb", "com.jflan.MiniKeePass.kdbx",
         "com.kptouch.kdb", "com.kptouch.kdbx",
         "com.markmcguill.strongbox.kdb",
