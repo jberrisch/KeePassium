e2cdf10892a4f0276eacea5cb4f939334afd598c
KeePassium <info@keepassium.com>
2019-12-14 19:03:58 +0100

    Chore: cherry-pick dismissable popover

---
+++ KeePassium/views/DismissableNavigationController.swift
@@ -0,0 +1,50 @@
+//  KeePassium Password Manager
+//  Copyright © 2018–2019 Andrei Popleteev <info@keepassium.com>
+// 
+//  This program is free software: you can redistribute it and/or modify it
+//  under the terms of the GNU General Public License version 3 as published
+//  by the Free Software Foundation: https://www.gnu.org/licenses/).
+//  For commercial licensing, please contact the author.
+
+import UIKit
+
+class DismissableNavigationController: UINavigationController {
+    
+    /// By default, the controller is created with a "Done" button on the right.
+    /// Alternatively, it can be a left "Cancel" button, or both —
+    /// but either button will dismiss the view controller.
+    init(
+        rootViewController: UIViewController,
+        leftButton: UIBarButtonItem.SystemItem? = nil,
+        rightButton: UIBarButtonItem.SystemItem? = .done)
+    {
+        super.init(rootViewController: rootViewController)
+        
+        if let leftButtonSystemItem = leftButton {
+            let leftButton = UIBarButtonItem(
+                barButtonSystemItem: leftButtonSystemItem,
+                target: self,
+                action: #selector(didPressButton))
+            rootViewController.navigationItem.leftBarButtonItem = leftButton
+        }
+        if let rightButtonSystemItem = rightButton {
+            let rightButton = UIBarButtonItem(
+                barButtonSystemItem: rightButtonSystemItem,
+                target: self,
+                action: #selector(didPressButton))
+            rootViewController.navigationItem.rightBarButtonItem = rightButton
+        }
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    @objc func didPressButton() {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
