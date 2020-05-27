22e644b857d233a2d67edfa28199b70b8e03d537
KeePassium <info@keepassium.com>
2020-05-28 01:09:06 +0200

    Fix(totp): support KeePassXC's "encoder" parameter for Steam TOTP (closes #101)

--- KeePassiumLib/KeePassiumLib/db/totp/TOTPGeneratorFactory.swift
+++ KeePassiumLib/KeePassiumLib/db/totp/TOTPGeneratorFactory.swift
@@ -71,6 +71,7 @@ fileprivate class GAuthFormat: SingleFieldFormat {
     static let lengthParam = "digits"
     static let algorithmParam = "algorithm"
     static let issuerParam = "issuer"
+    static let encoderParam = "encoder" // KeePassXC custom param for Steam
     
     static let defaultTimeStep = 30
     static let defaultLength = 6
@@ -116,7 +117,9 @@ fileprivate class GAuthFormat: SingleFieldFormat {
             return nil
         }
         
-        let isSteam = uriComponents.path.starts(with: "/Steam:") || (params[issuerParam] == "Steam")
+        let isSteam = uriComponents.path.starts(with: "/Steam:") ||
+            (params[issuerParam] == "Steam") ||
+            (params[encoderParam] == "steam")
         if isSteam {
             return TOTPGeneratorSteam(seed: ByteArray(data: seedData), timeStep: timeStep)
         }
