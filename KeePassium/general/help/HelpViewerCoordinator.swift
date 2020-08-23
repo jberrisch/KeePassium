16753d2ed0176a5ff322b119e659d05a87115e2c
KeePassium <info@keepassium.com>
2020-08-24 01:23:02 +0200

    Feat(premium): add help popover for perpetual fallback condition

--- KeePassium/general/help/HelpViewerCoordinator.swift
+++ KeePassium/general/help/HelpViewerCoordinator.swift
@@ -14,25 +14,13 @@ class HelpViewerCoordinator: NSObject, Coordinator {
     typealias DismissHandler = (HelpViewerCoordinator) -> Void
     var dismissHandler: DismissHandler?
     
-    fileprivate var navigationRouter: NavigationRouter
+    var article: HelpArticle?
+    
+    fileprivate var router: NavigationRouter
     private let helpViewerVC: HelpViewerVC
     
-    static func create(at popoverAnchor: PopoverAnchor) -> HelpViewerCoordinator {
-        let navVC = UINavigationController()
-        let navigationRouter = NavigationRouter(navVC)
-        let coordinator = HelpViewerCoordinator(navigationRouter: navigationRouter)
-        
-        navVC.modalPresentationStyle = .popover
-        navVC.presentationController?.delegate = coordinator
-        if let popover = navVC.popoverPresentationController {
-            popoverAnchor.apply(to: popover)
-            popover.delegate = coordinator
-        }
-        return coordinator
-    }
-    
-    init(navigationRouter: NavigationRouter) {
-        self.navigationRouter = navigationRouter
+    init(router: NavigationRouter) {
+        self.router = router
         
         helpViewerVC = HelpViewerVC.create()
         super.init()
@@ -41,42 +29,17 @@ class HelpViewerCoordinator: NSObject, Coordinator {
     }
     
     func start() {
-        fatalError("use start(in:) instead")
-    }
-    
-    func start(in parent: UIViewController) {
-        helpViewerVC.content = HelpArticle.perpetualFallbackLicense
-        navigationRouter.push(helpViewerVC, animated: true, onPop: {
-            [weak self] (viewController) in
-            guard let self = self else { return }
-            assert(viewController == self.helpViewerVC)
+        assert(article != nil)
+        helpViewerVC.content = article
+        router.push(helpViewerVC, animated: true, onPop: {
+            [self] (viewController) in // strong self
             self.dismissHandler?(self)
         })
-        parent.present(navigationRouter.navigationController, animated: true, completion: nil)
     }
 }
 
 extension HelpViewerCoordinator: HelpViewerDelegate {
     func didPressCancel(in viewController: HelpViewerVC) {
-        navigationRouter.pop(animated: true)
-    }
-}
-
-extension HelpViewerCoordinator: UIPopoverPresentationControllerDelegate {
-
-    func presentationController(
-        _ controller: UIPresentationController,
-        viewControllerForAdaptivePresentationStyle style: UIModalPresentationStyle
-        ) -> UIViewController?
-    {
-        return nil // "keep existing"
-    }
-}
-
-// MARK: UIAdaptivePresentationControllerDelegate
-
-extension HelpViewerCoordinator: UIAdaptivePresentationControllerDelegate {
-    func presentationControllerDidDismiss(_ presentationController: UIPresentationController) {
-        didPressCancel(in: helpViewerVC)
+        router.pop(animated: true)
     }
 }
